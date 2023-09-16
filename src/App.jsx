d1f59d818b9153d26660e4c166330ebc160fde2c
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 01:39:42 +0300

    change structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import Footer from "./components/footer/Footer.jsx";
-import List from "./components/list/List.jsx";
-
+import Home from "./pages/Home.jsx";
 function App() {
   return (
     <div>
-      <List />
-      <Footer />
+      <Home />
     </div>
   );
 }
