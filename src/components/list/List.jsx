614ef6929912089a0912e2f036722b7db76b5e94
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 01:38:15 +0300

    add AppBar, List and Footer to home

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,20 +2,23 @@ import ListHeader from "./list-header/ListHeader.jsx";
 import ListItem from "./list-item/ListItem.jsx";
 import ListFooter from "./list-footer/ListFooter.jsx";
 import "./list.css";
+import { Container } from "@mui/material";
 
 function List() {
   return (
-    <div className="todoapp">
-      <header>
-        <ListHeader />
-      </header>
-      <section>
-        <ListItem />
-      </section>
-      <footer>
-        <ListFooter />
-      </footer>
-    </div>
+    <Container>
+      <div className="todoapp">
+        <header>
+          <ListHeader />
+        </header>
+        <section>
+          <ListItem />
+        </section>
+        <footer>
+          <ListFooter />
+        </footer>
+      </div>
+    </Container>
   );
 }
 
