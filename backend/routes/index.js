433d94f0cf502b650af6c7dd2726ec73bdc986d4
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 23:29:18 +0300

    backend init and create routes

---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+import authRoutes from './auth.routes.js';
+import todoRoutes from './todo.routes.js';
+
+import isAuthenticated from '../middleware/is-authenticated.js';
+
+const router = express.Router();
+
+router.use('/auth', authRoutes);
+// To use routes below isAuthenticated you need to be an athenticated user
+router.use(isAuthenticated);
+router.use('/todos', todoRoutes);
+
+export default router;
