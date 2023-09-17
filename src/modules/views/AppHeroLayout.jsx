20277e0bd636b5af73c55bf9e7fdc9197049e55d
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 23:27:56 +0300

    add layout for AppHero

---
+++ src/modules/views/AppHeroLayout.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+
+const AppHeroLayoutRoot = styled("section")(({ theme }) => ({
+  color: theme.palette.common.white,
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    height: "80vh",
+    minHeight: 500,
+    maxHeight: 1300,
+  },
+}));
+
+function AppHeroLayout(props) {
+  const { children } = props;
+
+  return (
+    <AppHeroLayoutRoot>
+      <Container
+        sx={{
+          mt: 3,
+          mb: 14,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {children}
+        <Box
+          sx={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: "common.black",
+            opacity: 0.5,
+            zIndex: -1,
+          }}
+        />
+      </Container>
+    </AppHeroLayoutRoot>
+  );
+}
+
+AppHeroLayout.propTypes = {
+  children: PropTypes.node,
+  sxBackground: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+};
+
+export default AppHeroLayout;
