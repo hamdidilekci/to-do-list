2c67bdd299362cb77607948e52c4ff29deef0cf4
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 23:29:47 +0300

    create todo and user models

---
+++ backend/models/todo.model.js
@@ -0,0 +1,42 @@
+import mongoose, { model, Schema } from 'mongoose';
+
+// Todo Schema
+const TodoSchema = new Schema({
+    userId: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    priority: {
+        type: String,
+        default: null
+    },
+    category: {
+        type: String,
+        default: null
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: Date,
+    updatedAt: Date,
+}, {
+    autoCreate: true,
+    autoIndex: true,
+    timestamps: true,
+    versionKey: false,
+    collection: 'todo'
+});
+
+const Model = model('Todo', TodoSchema);
+
+export default Model;
