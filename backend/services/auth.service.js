d54b33027553a961c2df8374da4ea19dd9d6d461
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 23:30:04 +0300

    create auth and todo sevices

---
+++ backend/services/auth.service.js
@@ -0,0 +1,63 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/user.model.js';
+
+// commons
+import { BadRequestError } from '../common/errors.js';
+import checkPassword from '../common/check-password.js';
+import hashPassword from '../common/hash-password.js';
+
+export default class AuthService {
+    static async signIn({ email, password }) {
+
+        // check if user exists
+        const user = await User.findOne({ email }, ['password', '_id']);
+        if (!user) {
+            throw new BadRequestError('Email not found');
+        }
+        // check if password is valid
+        const validPassword = await checkPassword(password, user.password);
+        if (!validPassword) {
+            throw new BadRequestError('Invalid password');
+        }
+
+        if (email == null) {
+            throw new Error('Invalid credentials');
+        }
+
+        const _user = await User.findOne({ email }, ['_id', 'firstName', 'lastName', 'email', 'password', 'photo']);
+
+        // sign user in
+        return AuthService.signInDirect(_user);
+    }
+
+    static async signInDirect(user) {
+        const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN
+        });
+
+        const signInData = {
+            user,
+            accessToken,
+        };
+
+        return signInData;
+    }
+
+    static async signUp({password, email, ...data}) {
+        if (!password || !email) {
+            throw new BadRequestError('Paths `password` and `email` are required.');
+        }
+        if (await User.exists({ email })) {
+            throw new BadRequestError(`Email address is already in use: ${email}`);
+        }
+
+        // create user
+        const user = new User({ email, ...data });
+        user.password = await hashPassword(password);
+        await user.save();
+
+        return user;
+    }
+
+}
---
+++ backend/services/todo.service.js
@@ -0,0 +1,61 @@
+import Todo from '../models/todo.model.js';
+
+import { BadRequestError, NotFoundError } from '../common/errors.js';
+
+export default class TodoService {
+    static async create(currentUser, { title, description, priority, category }) {
+
+        if (!title || title === '') {
+            throw new BadRequestError('Title path is required when creating a todo item');
+        }
+
+        // create new todo item
+        const todo = new Todo({
+            userId: currentUser._id,
+            title,
+            description,
+            priority,
+            category
+        });
+
+        await todo.save();
+
+        return todo;
+    }
+
+    static async find(currentUser, sort, filter) {
+        // TODO: add filtering from request and combine two filters
+        let query = Todo.find({ userId: currentUser._id, ...filter });
+        if (sort) {
+            query = query.collation({ locale: 'en' }).sort(sort);
+        }
+
+        return query.exec();
+    }
+
+    static async update(currentUser, todoId, data) {
+        const filter = { userId: currentUser._id, _id: todoId };
+
+        // check if todo item exists
+        if (!Todo.exists(filter)) {
+            throw new NotFoundError('Todo item not found' );
+        }
+
+        // find & update todo item
+        const todoItem = await Todo.findOneAndUpdate(filter, data, {new: true});
+
+        return todoItem;
+    }
+
+    static async delete(currentUser, todoId) {
+        const filter = { userId: currentUser._id, _id: todoId };
+
+        // check if todo item exists
+        if (!Todo.exists(filter)) {
+            throw new NotFoundError('Todo item not found' );
+        }
+        await Todo.findOneAndDelete(filter)
+
+        return;
+    }
+}
