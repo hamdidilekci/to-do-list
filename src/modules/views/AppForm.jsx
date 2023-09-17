9d8f1d5d3b9d40c1937c4994b282cd11b59f844f
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-17 23:27:23 +0300

    add form component

---
+++ src/modules/views/AppForm.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "../../components/Paper.jsx";
+
+function AppForm(props) {
+  const { children } = props;
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+      }}
+    >
+      <Container maxWidth="sm">
+        <Box sx={{ mt: 7, mb: 12 }}>
+          <Paper sx={{ py: { xs: 4, md: 8 }, px: { xs: 3, md: 6 } }}>
+            {children}
+          </Paper>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+AppForm.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AppForm;
