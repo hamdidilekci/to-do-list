4029eabcb32f0d476cf72a1335b7d7221b2d56e9
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-18 23:34:51 +0300

    remove language select

--- src/modules/views/AppFooter.jsx
+++ src/modules/views/AppFooter.jsx
@@ -33,53 +33,17 @@ export default function AppFooter() {
   return (
     <Stack sx={footerContainerStyle}>
       <Container maxWidth="lg">
-        <Grid
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={2}
-        >
-          <Grid item xs={12} sm={4}>
-            <Stack sx={copyrightStyle}>
-              {"© "}
-              <Link
-                href="https://www.linkedin.com/in/dilekcihamdi/"
-                sx={{ color: "common.white" }}
-              >
-                Hamdi DİLEKÇİ{" "}
-              </Link>
-              {new Date().getFullYear()}
-            </Stack>
-          </Grid>
-
-          <Grid item xs={12} sm={4}>
-            <Grid container alignItems="center">
-              <Grid item xs={3}>
-                <Typography variant="subtitle1" sx={{ color: "common.white" }}>
-                  Language
-                </Typography>
-              </Grid>
-              <Grid item xs={3} sx={{ backgroundColor: "#656575" }}>
-                <TextField
-                  select
-                  size="small"
-                  variant="outlined"
-                  SelectProps={{
-                    native: true,
-                  }}
-                  sx={{
-                    width: "100%",
-                  }}
-                >
-                  {LANGUAGES.map((language) => (
-                    <option value={language.code} key={language.code}>
-                      {language.name}
-                    </option>
-                  ))}
-                </TextField>
-              </Grid>
-            </Grid>
-          </Grid>
+        <Grid item xs={12} sm={4}>
+          <Stack sx={copyrightStyle}>
+            {"© "}
+            <Link
+              href="https://www.linkedin.com/in/dilekcihamdi/"
+              sx={{ color: "common.white" }}
+            >
+              Hamdi DİLEKÇİ{" "}
+            </Link>
+            {new Date().getFullYear()}
+          </Stack>
         </Grid>
       </Container>
     </Stack>
