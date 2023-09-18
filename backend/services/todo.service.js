24ffd78f0468abce731ef6edb16845ff6b1d7bbb
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-18 23:29:33 +0300

    send response after delete

--- backend/services/todo.service.js
+++ backend/services/todo.service.js
@@ -1,61 +1,61 @@
-import Todo from '../models/todo.model.js';
+import Todo from "../models/todo.model.js";
 
-import { BadRequestError, NotFoundError } from '../common/errors.js';
+import { BadRequestError, NotFoundError } from "../common/errors.js";
 
 export default class TodoService {
-    static async create(currentUser, { title, description, priority, category }) {
-
-        if (!title || title === '') {
-            throw new BadRequestError('Title path is required when creating a todo item');
-        }
-
-        // create new todo item
-        const todo = new Todo({
-            userId: currentUser._id,
-            title,
-            description,
-            priority,
-            category
-        });
-
-        await todo.save();
-
-        return todo;
+  static async create(currentUser, { title, description, priority, category }) {
+    if (!title || title === "") {
+      throw new BadRequestError(
+        "Title path is required when creating a todo item"
+      );
     }
 
-    static async find(currentUser, sort, filter) {
-        // TODO: add filtering from request and combine two filters
-        let query = Todo.find({ userId: currentUser._id, ...filter });
-        if (sort) {
-            query = query.collation({ locale: 'en' }).sort(sort);
-        }
+    // create new todo item
+    const todo = new Todo({
+      userId: currentUser._id,
+      title,
+      description,
+      priority,
+      category,
+    });
 
-        return query.exec();
+    await todo.save();
+
+    return todo;
+  }
+
+  static async find(currentUser, sort, filter) {
+    // TODO: add filtering from request and combine two filters
+    let query = Todo.find({ userId: currentUser._id, ...filter });
+    if (sort) {
+      query = query.collation({ locale: "en" }).sort(sort);
     }
 
-    static async update(currentUser, todoId, data) {
-        const filter = { userId: currentUser._id, _id: todoId };
+    return query.exec();
+  }
 
-        // check if todo item exists
-        if (!Todo.exists(filter)) {
-            throw new NotFoundError('Todo item not found' );
-        }
+  static async update(currentUser, todoId, data) {
+    const filter = { userId: currentUser._id, _id: todoId };
 
-        // find & update todo item
-        const todoItem = await Todo.findOneAndUpdate(filter, data, {new: true});
-
-        return todoItem;
+    // check if todo item exists
+    if (!Todo.exists(filter)) {
+      throw new NotFoundError("Todo item not found");
     }
 
-    static async delete(currentUser, todoId) {
-        const filter = { userId: currentUser._id, _id: todoId };
+    // find & update todo item
+    const todoItem = await Todo.findOneAndUpdate(filter, data, { new: true });
 
-        // check if todo item exists
-        if (!Todo.exists(filter)) {
-            throw new NotFoundError('Todo item not found' );
-        }
-        await Todo.findOneAndDelete(filter)
+    return todoItem;
+  }
 
-        return;
+  static async delete(currentUser, todoId) {
+    const filter = { userId: currentUser._id, _id: todoId };
+
+    // check if todo item exists
+    if (!Todo.exists(filter)) {
+      throw new NotFoundError("Todo item not found");
     }
+
+    return Todo.findOneAndDelete(filter);
+  }
 }
