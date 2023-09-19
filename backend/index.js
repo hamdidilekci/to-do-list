edbf140a6b78c5f1b276469eb9d39942412ffabb
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-19 13:47:21 +0300

    set debug for routes

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,35 @@
-import express from 'express';
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
 import mongoose from "mongoose";
+mongoose.set("debug", true);
 
-import authenticate from './middleware/authenticate.js'
-import routes from './routes/index.js'
+import authenticate from "./middleware/authenticate.js";
+import routes from "./routes/index.js";
 
-const {DATABASE_USERNAME,DATABASE_PASSWORD, DATABASE_URL} = process.env
+const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_URL } = process.env;
 
-const connectionString = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_URL}`
+const connectionString = `mongodb+srv://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_URL}`;
 
-await mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true})
+await mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // allow empty strings
-mongoose.Schema.Types.String.checkRequired(v => typeof v === 'string');
+mongoose.Schema.Types.String.checkRequired((v) => typeof v === "string");
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(authenticate({ secret: process.env.JWT_SECRET }));
-app.use('/', routes);
-
+app.use("/", routes);
 
 app.listen(PORT, () => {
-    console.log(`app listening on http://localhost:${PORT}`);
-})
-
+  console.log(`app listening on http://localhost:${PORT}`);
+});
