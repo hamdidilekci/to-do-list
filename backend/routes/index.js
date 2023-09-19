e6463035b94e3cf592dd105230c8479ffaa1bcd6
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-19 13:48:57 +0300

    add reminder routes to route index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,17 @@
-import express from 'express';
+import express from "express";
 
-import authRoutes from './auth.routes.js';
-import todoRoutes from './todo.routes.js';
+import authRoutes from "./auth.routes.js";
+import todoRoutes from "./todo.routes.js";
+import reminderRoutes from "./reminder.routes.js";
 
-import isAuthenticated from '../middleware/is-authenticated.js';
+import isAuthenticated from "../middleware/is-authenticated.js";
 
 const router = express.Router();
 
-router.use('/auth', authRoutes);
+router.use("/auth", authRoutes);
 // To use routes below isAuthenticated you need to be an athenticated user
 router.use(isAuthenticated);
-router.use('/todos', todoRoutes);
+router.use("/todos", todoRoutes);
+router.use("/reminder", reminderRoutes);
 
 export default router;
