65f1b88f8f3b23490d11873cd9817ee0c85be77f
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-19 03:14:18 +0300

    add update routes,service

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,33 +1,41 @@
-import express from 'express';
+import express from "express";
 
-import AuthService from '../services/auth.service.js';
+import AuthService from "../services/auth.service.js";
 
-import handleError from '../middleware/handle-error.js'
+import handleError from "../middleware/handle-error.js";
 
 const router = express.Router();
 
-router.post('/sign-in', async(req, res) => {
-    try {
-        const signInData = await AuthService.signIn(req.body);
+router.post("/sign-in", async (req, res) => {
+  try {
+    const signInData = await AuthService.signIn(req.body);
 
-        res.send(signInData);
-    }
-    catch (error) {
-        handleError(error, req, res);
-    }
+    res.send(signInData);
+  } catch (error) {
+    handleError(error, req, res);
+  }
 });
 
-router.post('/sign-up', async(req, res) => {
-    try {
-        const user = await AuthService.signUp(req.body);
+router.post("/sign-up", async (req, res) => {
+  try {
+    const user = await AuthService.signUp(req.body);
 
-        // sign user in
-        const signInData = await AuthService.signInDirect(user);
-        res.send(signInData);
-    }
-    catch (error) {
-        handleError(error, req, res);
-    }
+    // sign user in
+    const signInData = await AuthService.signInDirect(user);
+    res.send(signInData);
+  } catch (error) {
+    handleError(error, req, res);
+  }
+});
+
+// update a todo item
+router.put("/update-profile", async (req, res) => {
+  try {
+    const user = await AuthService.update(req.body);
+    res.send(user);
+  } catch (error) {
+    handleError(error, req, res);
+  }
 });
 
 export default router;
--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,63 +1,78 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
 
 // commons
-import { BadRequestError } from '../common/errors.js';
-import checkPassword from '../common/check-password.js';
-import hashPassword from '../common/hash-password.js';
+import { BadRequestError } from "../common/errors.js";
+import checkPassword from "../common/check-password.js";
+import hashPassword from "../common/hash-password.js";
 
 export default class AuthService {
-    static async signIn({ email, password }) {
-
-        // check if user exists
-        const user = await User.findOne({ email }, ['password', '_id']);
-        if (!user) {
-            throw new BadRequestError('Email not found');
-        }
-        // check if password is valid
-        const validPassword = await checkPassword(password, user.password);
-        if (!validPassword) {
-            throw new BadRequestError('Invalid password');
-        }
-
-        if (email == null) {
-            throw new Error('Invalid credentials');
-        }
-
-        const _user = await User.findOne({ email }, ['_id', 'firstName', 'lastName', 'email', 'password', 'photo']);
-
-        // sign user in
-        return AuthService.signInDirect(_user);
+  static async signIn({ email, password }) {
+    // check if user exists
+    const user = await User.findOne({ email }, ["password", "_id"]);
+    if (!user) {
+      throw new BadRequestError("Email not found");
+    }
+    // check if password is valid
+    const validPassword = await checkPassword(password, user.password);
+    if (!validPassword) {
+      throw new BadRequestError("Invalid password");
     }
 
-    static async signInDirect(user) {
-        const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-            expiresIn: process.env.JWT_EXPIRES_IN
-        });
-
-        const signInData = {
-            user,
-            accessToken,
-        };
-
-        return signInData;
+    if (email == null) {
+      throw new Error("Invalid credentials");
     }
 
-    static async signUp({password, email, ...data}) {
-        if (!password || !email) {
-            throw new BadRequestError('Paths `password` and `email` are required.');
-        }
-        if (await User.exists({ email })) {
-            throw new BadRequestError(`Email address is already in use: ${email}`);
-        }
+    const _user = await User.findOne({ email }, [
+      "_id",
+      "firstName",
+      "lastName",
+      "email",
+      "password",
+      "photo",
+    ]);
 
-        // create user
-        const user = new User({ email, ...data });
-        user.password = await hashPassword(password);
-        await user.save();
+    // sign user in
+    return AuthService.signInDirect(_user);
+  }
 
-        return user;
+  static async signInDirect(user) {
+    const accessToken = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    const signInData = {
+      user,
+      accessToken,
+    };
+
+    return signInData;
+  }
+
+  static async signUp({ password, email, ...data }) {
+    if (!password || !email) {
+      throw new BadRequestError("Paths `password` and `email` are required.");
+    }
+    if (await User.exists({ email })) {
+      throw new BadRequestError(`Email address is already in use: ${email}`);
     }
 
+    // create user
+    const user = new User({ email, ...data });
+    user.password = await hashPassword(password);
+    await user.save();
+
+    return user;
+  }
+
+  static async update({ firstName, lastName, email }) {
+    const user = await User.findOneAndUpdate({ email, firstName, lastName });
+
+    if (!email) {
+      throw new BadRequestError("email not found");
+    }
+
+    return user;
+  }
 }
