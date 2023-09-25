8148de8561456ef588f80c88e986827b54e42ccc
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-26 00:47:05 +0300

    create AvatarUrlContext

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Swal from "sweetalert2";
-import Grid from "@mui/material/Grid";
-import Fab from "@mui/material/Fab";
+import { Grid, Fab, Box, Card } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { Field, Form, FormSpy } from "react-final-form";
 import Typography from "../components/Typography.jsx";
@@ -11,23 +10,37 @@ import FormButton from "../components/form/FormButton.jsx";
 import FormFeedback from "../components/form/FormFeedback.jsx";
 import { useBackend } from "../context/backend-context.jsx";
 import convertToBase64 from "../helpers/fileToBase64.js";
+import { useAvatar } from "../context/AvatarContext.jsx";
 
 const Profile = () => {
   const [sent, setSent] = React.useState(false);
+  const [selectedFile, setSelectedFile] = React.useState(null);
+  const { setAvatarUrl, avatarUrl } = useAvatar();
 
   const fileInputRef = React.createRef();
 
   const backend = useBackend();
+
+  // handle image input
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  let selectedFileUrl;
+  selectedFile && (selectedFileUrl = URL.createObjectURL(selectedFile));
 
   // Handle form submission
   const handleSubmit = async (values) => {
     setSent(true);
 
     const file = fileInputRef.current.files[0];
+
     if (file) {
       const base64File = await convertToBase64(file);
 
       values.avatar = base64File;
+      setAvatarUrl(base64File);
     }
 
     await backend
@@ -64,26 +77,64 @@ const Profile = () => {
             <Grid container spacing={2}>
               {/* Add file input for avatar */}
               <Grid item xs={12} sm={12}>
-                <label htmlFor="avatar">
-                  <input
-                    ref={fileInputRef}
-                    style={{ display: "none" }}
-                    id="avatar"
-                    name="avatar"
-                    type="file"
-                    accept=".jpg, .png, .jpeg"
-                  />
-
-                  <Fab
-                    color="secondary"
-                    size="small"
-                    component="span"
-                    aria-label="add"
-                    variant="extended"
-                  >
-                    <AddIcon /> Upload photo
-                  </Fab>
-                </label>
+                <Card
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    p: 2,
+                  }}
+                >
+                  <label htmlFor="avatar">
+                    <input
+                      ref={fileInputRef}
+                      style={{ display: "none" }}
+                      id="avatar"
+                      name="avatar"
+                      type="file"
+                      accept=".jpg, .png, .jpeg"
+                      onChange={handleFileChange}
+                    />
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        cursor: "pointer",
+                      }}
+                    >
+                      {selectedFile ? (
+                        <>
+                          <img
+                            src={selectedFileUrl}
+                            alt={selectedFile.name}
+                            width={150}
+                            height={150}
+                            style={{
+                              borderRadius: "50%",
+                              marginBottom: 1,
+                            }}
+                          />
+                          <Typography variant="subtitle1" color="textSecondary">
+                            {selectedFile.name}
+                          </Typography>
+                        </>
+                      ) : (
+                        <>
+                          <Fab
+                            color="secondary"
+                            size="small"
+                            component="span"
+                            aria-label="add"
+                            variant="extended"
+                          >
+                            <AddIcon /> Upload photo
+                          </Fab>
+                        </>
+                      )}
+                    </Box>
+                  </label>
+                </Card>
               </Grid>
               <Grid item xs={12} sm={6}>
                 <Field
