b78ecc1c7644de466ea7e655ecb9fd58ff04e416
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-26 14:47:07 +0300

    create puplic and private routes for auth

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 
-import authRoutes from "./auth.routes.js";
+import {
+  publicRouter as authPublicRoutes,
+  privateRouter as authPrivateRoutes,
+} from "./auth.routes.js";
 import todoRoutes from "./todo.routes.js";
 import reminderRoutes from "./reminder.routes.js";
 
@@ -8,9 +11,10 @@ import isAuthenticated from "../middleware/is-authenticated.js";
 
 const router = express.Router();
 
-router.use("/auth", authRoutes);
+router.use("/auth", authPublicRoutes);
 // To use routes below isAuthenticated you need to be an authenticated user
 router.use(isAuthenticated);
+router.use("/auth", authPrivateRoutes);
 router.use("/todos", todoRoutes);
 router.use("/reminder", reminderRoutes);
 
