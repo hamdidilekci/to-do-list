2b1a4ed8794ad03b589918c8757472c455342b99
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-09-26 14:51:36 +0300

    add upload file while signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
+import { Box, Grid, Link, Card, Fab } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 import { Field, Form, FormSpy } from "react-final-form";
 import Typography from "../components/Typography.jsx";
 import AppForm from "../components/layout/AppForm.jsx";
@@ -11,11 +10,24 @@ import RFTextField from "../components/form/RFTextField.jsx";
 import FormButton from "../components/form/FormButton.jsx";
 import FormFeedback from "../components/form/FormFeedback.jsx";
 import { useBackend } from "../context/backend-context.jsx";
+import convertToBase64 from "../helpers/fileToBase64.js";
 
 function SignUp() {
   const [sent, setSent] = React.useState(false);
   const navigateTo = useNavigate();
   const backend = useBackend();
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const fileInputRef = React.createRef();
+
+  // handle image input
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+  };
+
+  let selectedFileUrl;
+  selectedFile && (selectedFileUrl = URL.createObjectURL(selectedFile));
 
   const validate = (values) => {
     const errors = required(
@@ -35,6 +47,13 @@ function SignUp() {
 
   const handleSubmit = async (values) => {
     setSent(true);
+    const file = fileInputRef.current.files[0];
+
+    if (file) {
+      const base64File = await convertToBase64(file);
+
+      values.avatar = base64File;
+    }
     // Send a POST request to backend endpoint with the form values
     await backend
       .post("auth/sign-up", values, false)
@@ -76,6 +95,69 @@ function SignUp() {
               sx={{ mt: 6 }}
             >
               <Grid container spacing={2}>
+                <Grid item xs={12} sm={12}>
+                  <Card
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      p: 2,
+                    }}
+                  >
+                    <label htmlFor="avatar">
+                      <input
+                        ref={fileInputRef}
+                        style={{ display: "none" }}
+                        id="avatar"
+                        name="avatar"
+                        type="file"
+                        accept=".jpg, .png, .jpeg"
+                        onChange={handleFileChange}
+                      />
+                      <Box
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                          cursor: "pointer",
+                        }}
+                      >
+                        {selectedFile ? (
+                          <>
+                            <img
+                              src={selectedFileUrl}
+                              alt={selectedFile.name}
+                              width={150}
+                              height={150}
+                              style={{
+                                borderRadius: "50%",
+                                marginBottom: 1,
+                              }}
+                            />
+                            <Typography
+                              variant="subtitle1"
+                              color="textSecondary"
+                            >
+                              {selectedFile.name}
+                            </Typography>
+                          </>
+                        ) : (
+                          <>
+                            <Fab
+                              color="secondary"
+                              size="small"
+                              component="span"
+                              aria-label="add"
+                              variant="extended"
+                            >
+                              <AddIcon /> Upload photo
+                            </Fab>
+                          </>
+                        )}
+                      </Box>
+                    </label>
+                  </Card>
+                </Grid>
                 <Grid item xs={12} sm={6}>
                   <Field
                     autoFocus
