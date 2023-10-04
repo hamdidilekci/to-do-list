3c66d33b1035b99c77d3a28b7a9349662ecdb362
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2023-10-04 13:08:09 +0300

    add reset password verify path

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
+import ResetPasswordForm from "./pages/ResetPasswordForm.jsx";
 import Profile from "./pages/Profile.jsx";
 import AppFooter from "./components/layout/AppFooter.jsx";
 import ResponsiveAppBar from "./components/layout/ResponsiveAppBar.jsx";
@@ -14,17 +15,20 @@ import { BackendProvider } from "./context/backend-context.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* <ErrorHandler> */}
       <BackendProvider>
         <ResponsiveAppBar />
         <Routes>
           <Route path="Sign-in" element={<SignIn />} />
           <Route path="Sign-up" element={<SignUp />} />
-          <Route path="ForgotPassword" element={<ForgotPassword />} />
+          <Route path="Forgot-password" element={<ForgotPassword />} />
+          <Route path="Reset-password-verify" element={<ResetPasswordForm />} />
           <Route path="Profile" element={<Profile />} />
           <Route path="*" element={<App />} />
         </Routes>
         <AppFooter />
       </BackendProvider>
+      {/* </ErrorHandler> */}
     </BrowserRouter>
   </React.StrictMode>
 );
